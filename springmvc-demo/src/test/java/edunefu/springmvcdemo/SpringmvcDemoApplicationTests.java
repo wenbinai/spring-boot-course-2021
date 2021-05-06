2e6f051d91f86b662a953d253b85a6d2bab0e107
wenbinai <208176146@qq.com>
2021-05-06 09:53:46 +0800

    init environment

---
+++ springmvc-demo/src/test/java/edunefu/springmvcdemo/SpringmvcDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package edunefu.springmvcdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringmvcDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
