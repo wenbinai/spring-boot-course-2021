5ad4362593a72b2d86e691f05caf4a64c293aa46
wenbinai <208176146@qq.com>
2021-05-10 10:33:43 +0800

    添加MybatisPlus 乐观锁配置

---
+++ backend-examples/src/main/java/edu/nefu/backendexamples/configuration/MybatisPlusConfiguration.java
@@ -0,0 +1,15 @@
+package edu.nefu.backendexamples.configuration;
+
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.OptimisticLockerInnerInterceptor;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MybatisPlusConfiguration {
+
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        MybatisPlusInterceptor mybatisPlusInterceptor = new MybatisPlusInterceptor();
+        mybatisPlusInterceptor.addInnerInterceptor(new OptimisticLockerInnerInterceptor());
+        return mybatisPlusInterceptor;
+    }
+}
