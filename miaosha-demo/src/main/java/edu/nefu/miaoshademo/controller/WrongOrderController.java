f940a38114cb93f385681c2b641ce0767b5b9905
wenbinai <208176146@qq.com>
2021-05-16 10:38:07 +0800

    添加限流算法

--- miaosha-demo/src/main/java/edu/nefu/miaoshademo/controller/WrongOrderController.java
+++ miaosha-demo/src/main/java/edu/nefu/miaoshademo/controller/WrongOrderController.java
@@ -1,5 +1,6 @@
 package edu.nefu.miaoshademo.controller;
 
+import com.google.common.util.concurrent.RateLimiter;
 import edu.nefu.miaoshademo.service.StockOrderService;
 import edu.nefu.miaoshademo.vo.ResultVO;
 import lombok.extern.slf4j.Slf4j;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 @RestController
 @RequestMapping("/api/wrong/")
@@ -18,6 +20,12 @@ public class WrongOrderController {
     @Autowired
     private StockOrderService stockOrderService;
 
+    /**
+     * 仅仅使用乐观锁更新库存
+     *
+     * @param sid
+     * @return
+     */
     @PostMapping("order/{sid}")
     public ResultVO buy(@PathVariable("sid") int sid) {
         log.debug("购买的商品编号: {}", sid);
@@ -30,4 +38,33 @@ public class WrongOrderController {
         }
         return ResultVO.success(Map.of("id", id));
     }
+
+    // 每秒放行10个请求
+    RateLimiter rateLimiter = RateLimiter.create(10);
+
+    /**
+     * 通过令牌桶和限流算法更新库存
+     *
+     * @param sid
+     * @return
+     */
+    @PostMapping("orderByLimit/{sid}")
+    public ResultVO buyByLimit(@PathVariable("sid") int sid) {
+        if (!rateLimiter.tryAcquire(1000, TimeUnit.MILLISECONDS)) {
+            log.debug("你被限流了, 真不幸");
+            return ResultVO.fail(400, "购买失败, 库存不足");
+        }
+        log.debug("购买的商品编号: {}", sid);
+        int id = 0;
+        try {
+            id = stockOrderService.add(sid);
+            log.debug("创建订单id: {}", id);
+        } catch (Exception e) {
+            return ResultVO.fail(400, "购买失败, 库存不足");
+        }
+        return ResultVO.success(Map.of("id", id));
+    }
+
+    // TODO: 实现悲观锁+令牌桶更新库存
+
 }
